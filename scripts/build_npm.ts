4232c44e82ea456b5a5090a67576618bfa4a714b
ppdx999 <ppdx999@gmail.com>
2023-09-03 15:03:51 +0900

    improve npm build process: automatically set description and license

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -10,8 +10,9 @@ const [pkg, err] = loader(
   Deno.readTextFileSync("./NPM_PACKAGE").trim(),
   $env({
     version: $string,
+    owner: $string,
     name: $string,
-    repository: $optional($string),
+    repName: $optional($string),
   }),
 );
 
@@ -20,7 +21,19 @@ if (err) {
   Deno.exit(1);
 }
 
-const repository = pkg.repository ?? pkg.name;
+pkg.repName = pkg.repName ?? pkg.name;
+
+const apiUrl = `https://api.github.com/repos/${pkg.owner}/${pkg.repName}`;
+
+const result = await fetch(apiUrl);
+if (result.status !== 200) {
+  console.error(`Unable to fetch latest release from ${apiUrl}`);
+  Deno.exit(1);
+}
+
+const repo = await result.json();
+const description = repo.description;
+const license = repo.license.spdx_id;
 
 await emptyDir("./npm");
 
@@ -35,14 +48,14 @@ await build({
     // package.json properties
     name: pkg.name,
     version: pkg.version,
-    description: "Type safe env files loader written in deno",
-    license: "MIT",
+    description,
+    license,
     repository: {
       type: "git",
-      url: `git+https://github.com/ppdx999/${repository}.git`,
+      url: `git+https://github.com/${pkg.owner}/${pkg.repName}.git`,
     },
     bugs: {
-      url: `https://github.com/ppdx999/${repository}/issues`,
+      url: `https://github.com/${pkg.owner}/${pkg.repName}/issues`,
     },
   },
   postBuild() {
